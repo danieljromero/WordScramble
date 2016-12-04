d78ee7a41f9c4e2b12eeb0e5ba269276197f5475
Daniel Romero <dromsoft@users.noreply.github.com>
2016-12-03 19:15:35 -0500

    Update Scramble.py

--- code/Scramble.py
+++ code/Scramble.py
@@ -1,8 +1,24 @@
 # File: Scramble.py
 import random
 
-from Long import seven
+from Long import _seven
 
 # Random select
-select = random.choice(seven)
+select = random.choice(_seven)
 
+print "Original: " + select
+
+# Length seven
+prescramble = list(select)
+
+# Shuffle String
+random.shuffle(prescramble)
+
+# Create New String
+scramble = ''.join(prescramble)
+
+print "Scrambled: " + scramble
+print type(scramble)
+
+# New String to List of Chars
+_retVal = list(scramble)
