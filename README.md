dd1e3b79fadbad5e53a4703e724e1617ee8105a8
dromsoft <dromsoft9@gmail.com>
2017-01-27 21:07:38 -0500

    Implemented Click module, updated script, setup and README files

---
+++ README.md
@@ -0,0 +1,90 @@
+# WordScramble v0.1
+
+ A command line word game using Python3
+
+### Getting Started
+
+It is highly recommended to use a Python3 Virtual Environment (virtualenv) for managing dependencies, creating isolated python project environments and, most importantly, not breaking your system wide Python installation. Anyways, let's begin!
+
+### Prerequisites
+
+Check Python3 version, requires **Python 3.4+**
+```
+$ python3 --version
+```
+Check if **pip** is installed
+```
+$ pip --version
+```
+
+### Requirements
+
+This module requires:
+
+- Click 6.X (http://click.pocoo.org/6/)
+
+### Installation
+
+Use Virtual Environment (**if installed**)
+```
+$ workon somevirtualenv
+```
+Clone repository
+```
+$ git clone https://github.com/dromsoft/WordScramble.git
+```
+Navigate inside directory
+```
+$ cd WordScramble
+```
+Use pip
+```
+$ pip install --editable .
+```
+Check if installed correctly, you should see **WordScramble** listed
+```
+$ pip list
+```
+At this point you are ready to play the game!
+
+## Playing the game
+
+To see parameters needed for the game
+```
+$ wordscramble --help
+```
+- **DICTIONARY** parameter are any file with **.txt** extension that contain a list of words that you want to use.
+- **MAX_LENGTH** parameter is the highest word length you want use for the game
+    - It is recommended to use anything < 8, unless you're a pro
+
+### Example
+
+Move to **WordScramble** directory
+```
+$ cd ~/path/to/WordScramble/
+```
+Then do
+```
+$ wordscramble dictionaries/large.txt 4
+```
+Enjoy!
+
+## Uninstalling
+
+First do
+```
+$ pip uninstall wordscramble
+```
+
+Then delete directory
+```
+$ rm -rf ~/path/to/WordScramble/
+```
+
+## Contributing
+
+_Under Construction_
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- bin/wordscramble.py
+++ bin/wordscramble.py
@@ -1,61 +1,247 @@
 # File: wordscramble.py
-import sys
+import click
+import random
+import re
+from collections import OrderedDict
 
-sys.path.append("../wordscramble/")
+
+def extract(text_file):
+    """Creates a set of words from a text file
+
+    Opens and reads a text file, extracts the data and turns it into a stripped set of words to avoid any duplicates.
+    Program will yell at you if the text file has one or no words in it.
+
+    Args:
+        :param text_file: Instance of a file with (hopefully) extension .txt
+
+    Returns:
+        :return: A set of extracted words from text file
+
+    Raises:
+        :raise: ValueError: An error occurred accessing file contents
+        :raise: ValueError: Error occurred because file contents are minimal or non-existent
+    """
+    try:
+        file = open(text_file, 'r')  # open and read text file
+    except ValueError:
+        print("Cannot open text file, please try again")
+    else:
+        word_set = set([word.lower().rstrip() for word in file])  # avoid duplicates
+        file.close()  # close text file
+        if len(word_set) <= 1:
+            raise ValueError("\"{}\" does not contain enough words".format(text_file))
+        else:
+            return word_set
 
 
-def goodbye():
-    print("Goodbye...")
-    quit()
+def select_word(length, word_set):
+    """Randomly selects a word from a WordSet.
 
-if __name__ == "__main__":
+    Creates a list of all the words from a WordSet that match the length desired, then randomly selects one as the
+    original word for the game.
 
-    import create
-    import exists
-    import option
-    import game
+    Args:
+        :param length: An integer representing the length of the word to find
+        :param word_set: A set of words instance
 
-    path_lib = dict()
+    Returns:
+        :return: A randomly selected word from a list of words of a certain length
+    """
+    possible_words = [word for word in word_set if len(word) == length]  # list of words of a certain length
+    return random.choice(possible_words)  # returns random word from list
 
-    finished_loading = False
-    while finished_loading is False:
-        if option.existing_path_lib() is True:
-            existing = exists.load_json()
-            path_lib = existing.get_path_lib()
-            if option.display_path_lib_prompt() is True:
-                existing.display_path_lib()
-            finished_loading = True
-        elif option.create_path_lib_prompt() is True:
-            created = create.new_path_lib()
-            done = False
-            while not done:
-                created.prompt()
-                if create.add_another_path() is False:
-                    done = True
-            path_lib = created.get_path_lib()  # path library
-            if option.display_path_lib_prompt() is True:
-                created.display_path_lib()
-            if create.save_as_json_prompt() is True:
-                created.create_json()
+
+def scramble(word):
+    """Randomly orders a word.
+
+    Transforms word into a list of characters then shuffle's them and then reassembles the list back into a string.
+
+    Args:
+        :param word: The string to scramble
+
+    Returns:
+        :return: Randomly ordered string
+    """
+    char_list = list(word)  # list of characters in a word
+    random.shuffle(char_list)  # shuffles word
+    scrambled = ''.join(char_list)  # reassembles string
+    return scrambled
+
+
+def analyze(word):
+    """Calculates the amount of occurances for each letter in a word.
+
+    Creates a dictionary for each letter in the word along with its amount of repetitions.
+
+    Args:
+        :param word: string to analyze
+
+    Return:
+        :return: a dictionary, dict() object, with each letter in a word as keys and the amount of repetitions as values
+    """
+    word_list = list(enumerate(word))  # creates a list of occurrences
+    results = dict()  # dictionary to store keys, and values
+    for index, letter in word_list:
+        count = 1  # every letter in word occurs at least once
+        for i, character in word_list:
+            if i != index and letter == character:
+                count += 1
+        if letter not in results:
+            results.update({letter: count})
+    return results
+
+
+def valid_occurrences(word_stats, main_stats):
+    """Checks if each the word contains the correct number of letter repetitions.
+
+    Returns true if word contains correct number of characters based on word stats, false if otherwise.
+
+    Args:
+        :param word_stats: dict() of letters and their repetitions
+        :param main_stats: dict() of stats from the chosen word we are comparing
+
+    Return:
+        :return: True if word falls within the correct parameters, False if otherwise
+    """
+    for key in word_stats:
+        main_value = main_stats.get(key)
+        new_value = word_stats.get(key)
+        if new_value > main_value:
+            return False
+    return True
+
+
+def find_possible_words(selected_word, word_list, max_length):
+    """Uses the selected word and finds the words that can be used.
+
+    Uses the word to compare other words too with the correct lengths.
+
+    Args:
+        :param selected_word: word we are comparing
+        :param word_list: list of words
+        :param max_length: difficulty
+
+    Return:
+        :return: Set of possible words
+    """
+    main_stats = analyze(selected_word)  # gets the dictionary representation of letters and number of repetition
+    pw = [word for word in word_list if len(word) <= max_length and not set(word) - set(selected_word)]
+    possible_words = set([word for word in pw if valid_occurrences(analyze(word), main_stats) is True])
+    return possible_words
+
+
+def hide_word(word):
+    """Creates a word by creating an underscore representation.
+
+    Uses the length of the string and then creates a string of underscores for the word.
+
+    Args:
+        :param word: string we are planning to hide
+
+    Return:
+        :return: sequence of underscores based on string length
+    """
+    underscore = "_" * len(word)
+    return underscore
+
+
+def dictionary_of_answers(word_set):
+    """Uses a set of words and associates it with a dictionary.
+
+    Creates a dictionary with the key representing the correct words and the value as the underscore representation.
+
+    Args:
+        :param word_set: set of possible words
+
+    Return:
+        :return: Ordered dictionary of keys: answers, value: underscore representation
+    """
+    sort_alphabetically = sorted(word_set)  # sorts alphabetically
+    sort_by_length = sorted(sort_alphabetically, key=len)  # sorts by length
+    dashes = [hide_word(word) for word in sort_by_length]  # generates list of underscore's
+    answers = OrderedDict(zip(sort_by_length, dashes))  # preserves ordering for display
+    return answers
+
+
+def string_decorator(string):
+    """A decorator for reuse.
+
+    Decorates a print() function.
+
+    Args:
+        :param string: string to decorate
+    """
+    print("=" * len(string))
+    print(string)
+    print("=" * len(string))
+
+
+def game(answers, word):
+    """Actual game
+
+    Checks if the game is over, checks guesses, keep tracks of incorrect guesses and other functions.
+
+    Args:
+        :param answers: dictionary of correct words
+        :param word: word we are unscrambling
+    """
+    game_over = len(answers)  # number of answers to get correct
+    correct = set()  # holds correct guesses
+    incorrect = set()  # holds incorrect guesses
+
+    print("Press \"q\" to quit, \"v\" to view incorrect guesses")  # prompt option to quit
+
+    while len(correct) < game_over:  # if the number of correct guesses do not match the number of correct answers
+        for value in answers:
+            print(answers.get(value))
+        string_decorator("Unscramble: \""+word+"\"")  # word to unscramble
+
+        guess_prompt = "-> Guess Word: "
+        guess = input(guess_prompt)
+
+        while not guess:  # null check
+            guess = input(guess_prompt)
+
+        if guess in answers and guess not in correct:
+            correct.add(guess)  # adds correct guess to correct set()
+            answers[guess] = guess  # shows the correct guess
+        elif guess == "v":  # prompts for view
+            if incorrect:
+                string_decorator("Previous Guesses: " + str(incorrect))
             else:
-                if create.not_saved_json_prompt() is False:
-                    created.create_json()
-            finished_loading = True
-        elif option.use_default_prompt() is True:
-            path_lib = exists.use_default()
-            finished_loading = True
-        else:
-            if option.quit_game_prompt() is True:
-                goodbye()
+                string_decorator("Previous Guesses: None")
+        elif guess == "q":  # prompts to quit and game over
+            for key in answers:
+                print(key)
+            string_decorator("Game Over")
+            break
+        else:  # adds guess to incorrect
+            incorrect.add(guess)
 
-    play_again = True
-    while play_again is True:
-        if game.start_prompt() is True:
-            new_game = game.word_scramble(path_lib)
-            new_game.use_all()
-            new_game.start()
-            if game.play_again_prompt() is False:
-                goodbye()
-        else:
-            if option.quit_game_prompt() is True:
-                goodbye()
+    print("Correct Guesses: " + str(len(correct)) + "/" + str(game_over))  # shows result from game
+    if len(correct) == game_over:  # winner prompt
+        print("Congratulations! You win!")
+
+
+@click.command()
+@click.argument('dictionary', type=click.Path(exists=True))
+@click.argument('max_length', type=int)
+def main(dictionary, max_length):
+    """Command Line Word Game using Python3 and dictionaries"""
+    initial_set = extract(dictionary)  # set of words, no duplicates
+    actual_length = max(len(word) for word in initial_set)  # finds longest word length, int
+    if max_length <= actual_length:  # checks if user input: "max_length" is within range
+        original_word = select_word(max_length, initial_set)  # uses max_length to find word
+    else:
+        original_word = select_word(actual_length, initial_set)  # calculates and uses the actual largest length
+    pattern = "^["+original_word+"]{1,"+str(len(original_word))+"}$"  # "^[xyz]{1,n}" checks first character and length
+    regex = re.compile(pattern, re.IGNORECASE)
+    results = [match.group(0) for word in initial_set for match in [regex.match(word)] if match]
+    scrambled = scramble(original_word)
+    if scrambled == original_word:  # makes sure words aren't similar
+        while scrambled == original_word:
+            scrambled = scramble(original_word)
+    possible_word_set = find_possible_words(scrambled, results, max_length)
+    #print("Answers: " + str(possible_word_set))  # to see answers
+    answers = dictionary_of_answers(possible_word_set)
+    game(answers, scrambled)  # starts game
