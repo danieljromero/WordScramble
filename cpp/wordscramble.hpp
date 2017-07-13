ffc09f75ee5a3a23dfdf9d8cc9be733a452f5120
Daniel Romero <dromsoft9@gmail.com>
2017-07-13 00:26:25 -0400

    updating headerfile

--- cpp/wordscramble.hpp
+++ cpp/wordscramble.hpp
@@ -7,16 +7,16 @@
 
 class WordScramble {
 private:
-    std::map<std::string, int> extract(std::ifstream inputFile);
-    std::string selectRandomWord(const std::map<std::string, int>& map);
-    std::string scrambleWord(const std::string& word);
-    std::map<std::string, std::vector<int>> analyze(const std::string& word);
-    bool operator==(const std::string& word);
-    std::map<std::string, int> match(
-      std::string chosenWord,
-      const std::map<std::string, std::vector<int>>& wordBank,
-      int length);
-    WordScramble();
+  std::map<std::string, int> extract(std::ifstream inputFile);  // creates a map using the words keys and lengths as values
+  std::string selectRandomWord(const std::map<std::string, int>& map);  // selects a random word from the map
+  std::string scrambleWord(const std::string& word);  // scrambles word
+  std::map<std::string, std::vector<int>> analyze(const std::string& word);  // records number of letter occurrances for each word
+  bool operator==(const std::string& word);  // checks if the number of letters in a word is valid to use
+  std::map<std::string, int> match(std::string chosenWord,
+				   const std::map<std::string, std::vector<int>>& wordBank,
+				   int length
+				   );
+  WordScramble();  // removes default constructor
 
 
 public:
